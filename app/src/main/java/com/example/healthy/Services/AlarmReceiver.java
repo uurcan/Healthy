8e3d870a0381eaa55ca177dcbd278783e1d4e86f
ugurcancelebi <>
2019-06-02 18:31:44 +0300

    Alarm vibration implemented

--- app/src/main/java/com/example/healthy/Services/AlarmReceiver.java
+++ app/src/main/java/com/example/healthy/Services/AlarmReceiver.java
@@ -3,6 +3,7 @@ package com.example.healthy.Services;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
 
 import com.example.healthy.UI.Reminder.Alert;
 
@@ -12,6 +13,8 @@ public class AlarmReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         String title = intent.getStringExtra("title");
         Intent newIntent = new Intent(context, Alert.class);
+        Vibrator vibrator = (Vibrator)context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(10000);
         newIntent.putExtra("title",title);
         newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(newIntent);
